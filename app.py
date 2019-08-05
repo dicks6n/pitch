d486fbbfce8dd063f1d3cb222f0e250d1b18689d
dicks6n <dikkymccria@gmail.com>
2019-08-04 19:50:31 -0700

    adding pitches

--- app.py
+++ app.py
@@ -12,6 +12,31 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# Articles
+@app.route('/articles')
+def articles():
+    # Create cursor
+    cur = mysql.connection.cursor()
+
+    # Get articles
+    result = cur.execute("SELECT * FROM articles")
+
+    articles = cur.fetchall()
+
+    if result > 0:
+        return render_template('articles.html', articles=articles)
+    else:
+        msg = 'No Articles Found'
+        return render_template('articles.html', msg=msg)
+    # Close connection
+    cur.close()
+
+
+
+
+
+
 if __name__ == '__main__':
 
     app.run(debug=True)
